a745f324cb6a3413b833bd482b15204dd8928f65
Evgeniy <evgenuel@gmail.com>
2025-04-14 23:55:11 +0300

    Russian localisation, minor changes

--- init.lua
+++ init.lua
@@ -20,6 +20,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 local title = "Death Messages"
 local version = "0.1.4"
 local mname = "death_messages"
+local S = minetest.get_translator("death_messages")
 --------------------------------------------------------------------------------
 dofile(minetest.get_modpath("death_messages").."/settings.txt")
 --------------------------------------------------------------------------------
@@ -30,40 +31,40 @@ local messages = {}
 
 -- Lava death messages
 messages.lava = {
-    " melted into a ball of fire.",
-    " thought lava was cool.",
-    " melted into a ball of fire.",
-    " couldn't resist that warm glow of lava.",
-    " dug straight down.",
-    " didn't know lava was hot."
+    "melted into a ball of fire.",
+    "thought lava was cool.",
+    "melted into a ball of fire.",
+    "couldn't resist that warm glow of lava.",
+    "dug straight down.",
+    "didn't know lava was hot."
 }
 
 -- Drowning death messages
 messages.water = {
-    " drowned.",
-    " ran out of air.",
-    " failed at swimming lessons.",
-    " tried to impersonate an anchor.",
-    " forgot he wasn't a fish.",
-    " blew one too many bubbles."
+    "drowned.",
+    "ran out of air.",
+    "failed at swimming lessons.",
+    "tried to impersonate an anchor.",
+    "forgot he wasn't a fish.",
+    "blew one too many bubbles."
 }
 
 -- Burning death messages
 messages.fire = {
-    " burned to a crisp.",
-    " got a little too warm.",
-    " got too close to the camp fire.",
-    " just got roasted, hotdog style.",
-    " got burned up. More light that way."
+    "burned to a crisp.",
+    "got a little too warm.",
+    "got too close to the camp fire.",
+    "just got roasted, hotdog style.",
+    "got burned up. More light that way."
 }
 
 -- Other death messages
 messages.other = {
-    " died.",
-    " did something fatal.",
-    " gave up on life.",
-    " is somewhat dead now.",
-    " passed out -permanently."
+    "died.",
+    "did something fatal.",
+    "gave up on life.",
+    "is somewhat dead now.",
+    "passed out -permanently."
 }
 
 function get_message(mtype)
@@ -77,23 +78,23 @@ end
 minetest.register_on_dieplayer(function(player)
     local player_name = player:get_player_name()
     local node = minetest.registered_nodes[
-        minetest.get_node(player:getpos()).name
+        minetest.get_node(player:get_pos()).name
     ]
     if minetest.is_singleplayer() then
         player_name = "You"
     end
     -- Death by lava
     if node.groups.lava ~= nil then
-        minetest.chat_send_all(player_name .. get_message("lava"))
+        minetest.chat_send_all(player_name .. " " .. S(get_message("lava")))
     -- Death by drowning
     elseif player:get_breath() == 0 then
-        minetest.chat_send_all(player_name .. get_message("water"))
+        minetest.chat_send_all(player_name .. " " .. S(get_message("water")))
     -- Death by fire
     elseif node.name == "fire:basic_flame" then
-        minetest.chat_send_all(player_name .. get_message("fire"))
+        minetest.chat_send_all(player_name .. " " .. S(get_message("fire")))
     -- Death by something else
     else
-        minetest.chat_send_all(player_name .. get_message("other"))
+        minetest.chat_send_all(player_name .. " " .. S(get_message("other")))
     end
 
 end)
